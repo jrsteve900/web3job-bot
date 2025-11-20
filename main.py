4e757d6ac825f2788070787f1e8376c02d42ed8e
jrsteve900 <sjr28330@gmail.com>
2025-11-21 04:14:08 +0800

    Add basic Web3 Job Bot functionality

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+
+def run_bot():
+    print("Web3 Job Bot Running...")
+    while True:
+        print("Checking tasks...")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    run_bot()
